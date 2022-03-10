c7460a3ce117de5070a68997ad0be3a83e7d6825
Alua <lauraortsramon@gmail.com>
2022-03-10 13:22:31 +0100

    creacion clase Usuario

--- Reto_Digimon/src/reto_digimon/Usuario.java
+++ Reto_Digimon/src/reto_digimon/Usuario.java
@@ -11,11 +11,61 @@ package reto_digimon;
  */
 public class Usuario {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    private String nombreUsu;
+    private String pass;
+    private int pJugadas;
+    private int partidasGan;
+    private int cantTokens;
+    
+    public Usuario(String nombre, String contrasenya, int p_jugadas, int p_ganadas, int tokens) {
+        nombreUsu = nombre;
+        pass = contrasenya;
+        pJugadas = p_jugadas;
+        partidasGan = p_ganadas;
+        cantTokens = tokens;
     }
+
+
+    public String getNombreUsu() {
+        return nombreUsu;
+    }
+
+    public void setNombreUsu(String nombre) {
+        nombreUsu = nombre;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String contrasenya) {
+        pass = contrasenya;
+    }
+
+    public int getpJugadas() {
+        return pJugadas;
+    }
+
+    public void setpJugadas(int p_jugadas) {
+        pJugadas = p_jugadas;
+    }
+
+    public int getPartidasGan() {
+        return partidasGan;
+    }
+
+    public void setPartidasGan(int p_ganadas) {
+        partidasGan = p_ganadas;
+    }
+
+    public int getCantTokens() {
+        return cantTokens;
+    }
+
+    public void setCantTokens(int tokens) {
+        cantTokens = tokens;
+    }
+
     
 }
