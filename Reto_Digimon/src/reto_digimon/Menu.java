88e81eff57c0b92cdbe89c5c252288323022257a
Alvaro <Alvaro@192.168.101.88>
2022-03-17 12:15:33 +0100

    Modificacion menu

--- Reto_Digimon/src/reto_digimon/Menu.java
+++ Reto_Digimon/src/reto_digimon/Menu.java
@@ -5,38 +5,39 @@
  */
 package reto_digimon;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alvaro
  */
 public class Menu {
 
-   
-   public String AltaUsuario="";
-   public String Contraseña="";
-   
-   public Menu (String AltaU,String Contra){
-   
-   AltaUsuario=AltaU;
-   Contraseña=Contra;
-   
-   }
-   
-    public String getAltaUsuario() {
-        return AltaUsuario;
+    public static void main(String[] args) {
+
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion;
+
+        System.out.println("1, Iniciar sesion");
+        System.out.println("2, Dar de alta un usuario");
+        System.out.println("3, Crear un Digimon");
+        System.out.println("4, Definir Digievolucion");
+
+        System.out.println("Introduce tu eleccion");
+        opcion = sn.nextInt();
+
+        switch (opcion) {
+
+            case 1:
+                Digimon();
+                break;
+
+            default:
+                System.out.print("repetir");
+                break;
+        }
+
     }
 
-    public void setAltaUsuario(String AltaUsuario) {
-        this.AltaUsuario = AltaUsuario;
-    }
-
-    public String getContraseña() {
-        return Contraseña;
-    }
-
-    public void setContraseña(String Contraseña) {
-       this.Contraseña = Contraseña;
-    }
-   
-   
 }
