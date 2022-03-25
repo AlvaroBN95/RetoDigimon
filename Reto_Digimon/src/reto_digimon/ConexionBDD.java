bd7645f18a91e597b6c5a5ad052d1079e1586c45
Alvaro <Alvaro@DESKTOP-1O30C0R>
2022-03-25 10:37:28 +0100

    eliminar comentarios2

--- Reto_Digimon/src/reto_digimon/ConexionBDD.java
+++ Reto_Digimon/src/reto_digimon/ConexionBDD.java
@@ -7,9 +7,7 @@ package reto_digimon;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class ConexionBDD {
 
